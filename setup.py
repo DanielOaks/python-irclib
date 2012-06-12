cf3aaf9b097cf50fec8e981802f6abd5c54e5b8d
Daniel Oakley <danneh@danneh.net>
2012-06-13 09:57:57 +1000

    Made it play nicely with py2 as well

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 #! /usr/bin/env python
 
+import sys
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-setup(name="python-irclib3",
+if sys.version_info >= (3, 0):
+       package_dir = {'': 'src3'}
+else:
+       package_dir = {'': 'src2'}
+
+setup(name="python-irclib",
       version="0.4.8",
       py_modules=["irclib", "ircbot"],
+      package_dir=package_dir,
       author="Joel Rosdahl",
       author_email="joel@rosdahl.net",
       url="http://python-irclib.sourceforge.net")
